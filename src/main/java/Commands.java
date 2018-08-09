d3d06e466c049dccd328eee3eef3459b364614c6
AlexeevAlexandr <https://github.com/AlexeevAlexandr>
2018-08-09 18:02:40 +0300

    added sortList

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -4,89 +4,89 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 class Commands {
     Integer addDeveloper(String firstName, String lastName, String specialty, int experience) {
         Integer developerId;
-        try(SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            Session session = sessionFactory.openSession())
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+             Session session = sessionFactory.openSession())
         {
+            System.out.println("Adding Developer's records to the database");
             Transaction transaction;
             transaction = session.beginTransaction();
             Developer developer = new Developer(firstName, lastName, specialty, experience);
             developerId = (Integer) session.save(developer);
             transaction.commit();
             return developerId;
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Exception in add block: " + e.getMessage());
         }
         return 0;
     }
 
     void listDevelopers() {
-        try(SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            Session session = sessionFactory.openSession())
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+             Session session = sessionFactory.openSession())
         {
             List developers = session.createQuery("FROM Developer").list();
+            System.out.println("List of all Developers:");
             for (Object developer : developers) {
                 System.out.println(developer);
                 System.out.println("================");
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Exception in listByParameter block: " + e.getMessage());
         }
     }
 
     void updateDeveloper(int developerId, int experience) {
-        try(SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            Session session = sessionFactory.openSession())
-        {
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             Developer developer = session.get(Developer.class, developerId);
             developer.setExperience(experience);
             session.update(developer);
             transaction.commit();
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Exception in update block: " + e.getMessage());
         }
     }
 
     void removeDeveloper(int developerId) {
-        try(SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            Session session = sessionFactory.openSession())
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+             Session session = sessionFactory.openSession())
         {
             Transaction transaction = session.beginTransaction();
             Developer developer = session.get(Developer.class, developerId);
             session.delete(developer);
             transaction.commit();
-        }catch (Exception e){
+            System.out.println("Removing Some Developer and updating Proselyte Developer's experience:");
+        } catch (Exception e) {
             System.out.println("Exception in remove block: " + e.getMessage());
         }
     }
 
-    void deleteAllData(){
-        try(SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            Session session = sessionFactory.openSession())
-        {
+    void deleteAllData() {
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             session.createQuery("DELETE FROM Developer").executeUpdate();
             transaction.commit();
             System.out.println("Delete from table is successful");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Exception in deleteAllData block: " + e.getMessage());
         }
     }
 
-    void listByParameter(){
-        try(SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            Session session = sessionFactory.openSession())
+    void listByParameter() {
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+             Session session = sessionFactory.openSession())
         {
-            Query query = session.createQuery("FROM Developer D WHERE D.firstName = 'Developer2'");
-            List developers = query.list();
+            System.out.println("The Developer who will be deleted");
+            List developers = session.createQuery("FROM Developer D WHERE D.firstName = 'Developer2'").list();
             System.out.println(developers);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Exception in listByParameter block: " + e.getMessage());
         }
     }
@@ -96,16 +96,32 @@ class Commands {
      * (experience > 3) - points that list will be shown with experience more than 3;
      * (ORDER BY D.experience DESC) - sorting of list (DESC - by descending; ASC - by ascending);
      */
-    void sortList(){
-        try(SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            Session session = sessionFactory.openSession())
+    void sortList() {
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+             Session session = sessionFactory.openSession())
         {
-            Query query = session.createQuery(
-                    "FROM Developer D WHERE experience > 3 ORDER BY D.experience DESC");
-            List developers = query.list();
-            System.out.println(developers);
-        }catch (Exception e){
+            List developers = session.createQuery(
+                    "FROM Developer D WHERE experience > 3 ORDER BY D.experience DESC").list();
+            System.out.println("Sorted list");
+            for(Object developer : developers) {
+                System.out.println(developer);
+            }
+        } catch (Exception e) {
             System.out.println("Exception in sortList block: " + e.getMessage());
+        }
+    }
+
+    void groupList() {
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+             Session session = sessionFactory.openSession())
+        {
+            List developers = session.createQuery("FROM Developer D GROUP BY D.specialty").list();
+            System.out.println("grouping list");
+            for (Object developer : developers) {
+                System.out.println(developer);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception in groupList: " + e.getMessage());
         }
     }
 }
--- src/main/java/DeveloperRunner.java
+++ src/main/java/DeveloperRunner.java
@@ -1,32 +1,28 @@
 public class DeveloperRunner {
-
     public static void main(String[] args) {
         Commands commands = new Commands();
+        //clear table
         commands.deleteAllData();
-
-        System.out.println("Adding Developer's records to the database");
+        //add to table
         Integer developerId1 = commands.addDeveloper("Developer1", "Developer1", "Java Developer", 2);
         Integer developerId2 = commands.addDeveloper("Developer2", "Developer2", "C++ Developer", 2);
         commands.addDeveloper("Developer6", "Developer6", "Java Team Lead", 6);
         commands.addDeveloper("Developer4", "Developer4", "Java Junior", 1);
         commands.addDeveloper("Developer3", "Developer3", "Java Team Lead", 8);
         commands.addDeveloper("Developer5", "Developer5", "Java Team Lead", 4);
-
-        System.out.println("List of all Developers:");
+        //view list
         commands.listDevelopers();
-
-        System.out.println("The Developer who will be deleted");
+        //view list by parameter
         commands.listByParameter();
-
-        System.out.println("Removing Some Developer and updating Proselyte Developer's experience:");
+        //remove and update
         commands.removeDeveloper(developerId2);
         commands.updateDeveloper(developerId1, 3);
 
-        System.out.println("Final listByParameter of Developers:");
         commands.listDevelopers();
-
-        System.out.println("Sorted list");
+        //view sorted list
         commands.sortList();
+        //view group list
+        commands.groupList();
     }
 
 }
