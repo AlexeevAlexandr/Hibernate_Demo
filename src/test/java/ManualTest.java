4b0a1dbf9cf77c654d56eea57e8e185710b4b657
AlexeevAlexandr <https://github.com/AlexeevAlexandr>
2018-08-14 15:14:37 +0300

    added command dropCountID

--- src/test/java/ManualTest.java
+++ src/test/java/ManualTest.java
@@ -1,10 +1,11 @@
-public class qq {
+public class ManualTest {
     public static void main(String[] args) {
         Commands commands = new Commands();
 
         commands.SessionFactoryOpen();
+        System.out.println(commands.message.equals("id is reset")? "OK" : "False");
         commands.deleteAllData();
-        System.out.println(commands.message.equals("Delete from table is successful")? "OK" : "Error");
+        System.out.println(commands.message.equals("Delete from table is successful")? "OK" : "False");
 
         commands.dropCountID();
         commands.SessionFactoryClosed();
